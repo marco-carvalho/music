588f8a4eb74ae4f81ebb9dde1238044802276794
Marco Carvalho <marcolucio27@gmail.com>
2018-04-28 20:24:13 -0300

    eslint src --fix

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome";
 import "@fortawesome/fontawesome-free-solid/";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App)
+}).$mount("#app");
