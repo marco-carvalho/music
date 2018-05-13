b24096be2883e924a7966970104dca5f70db2f82
Marco Carvalho <marcolucio27@gmail.com>
2018-05-12 23:27:55 -0300

    add css classes

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,28 @@
 <template lang="pug">
     router-view
 </template>
+
+<style lang="sass">
+.btn
+  &-red
+    background-color: var(--red)
+    color: #fff
+  &-orange
+    background-color: var(--orange)
+    color: #fff
+  &-yellow
+    background-color: var(--yellow)
+    color: #fff
+  &-green
+    background-color: var(--green)
+    color: #fff
+  &-blue
+    background-color: var(--blue)
+    color: #fff
+  &-purple
+    background-color: var(--purple)
+    color: #fff
+  &-pink
+    background-color: var(--pink)
+    color: #fff
+</style>
