ba24ded5ae9079699273ed2e6d48891885f23b7a
Marco Carvalho <marcolucio27@gmail.com>
2018-06-01 18:15:02 -0300

    update colors

--- src/App.vue
+++ src/App.vue
@@ -9,28 +9,28 @@
 .text-shadow
   text-shadow: 1px 1px 1px #000
 .bg
+  &-white
+    background-color: var(--white)
+  &-white-red
+    +stripes(var(--white), var(--red))
   &-red
     background-color: var(--red)
   &-red-orange
     +stripes(var(--red), var(--orange))
   &-orange
     background-color: var(--orange)
-  &-orange-yellow
-    +stripes(var(--orange), var(--yellow))
   &-yellow
     background-color: var(--yellow)
+  &-yellow-green
+    +stripes(var(--yellow), var(--green))
   &-green
     background-color: var(--green)
   &-green-blue
-    +stripes(var(--red), var(--orange))
+    +stripes(var(--green), var(--blue))
   &-blue
     background-color: var(--blue)
   &-blue-purple
     +stripes(var(--blue), var(--purple))
   &-purple
     background-color: var(--purple)
-  &-purple-pink
-    +stripes(var(--purple), var(--pink))
-  &-pink
-    background-color: var(--pink)
 </style>
