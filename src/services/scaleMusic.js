05abd2015854b60c9b67efcc7f9fefb4aee48b3a
Bernardo <bernardo@csu.fullerton.edu>
2018-09-02 13:37:22 -0300

    adicionado variavel de ambiente para ao rodar o docker a aplicação funcionar totalmente offline

--- src/services/scaleMusic.js
+++ src/services/scaleMusic.js
@@ -1,5 +1,11 @@
 const axios = require('axios');
+let url = 'https://scalemusicapi.herokuapp.com'
 
-module.exports = axios.create({
-  baseURL: 'https://scalemusicapi.herokuapp.com',
-});
+if (process.env.VUE_APP_APIURL != undefined) {
+  url = process.env.VUE_APP_APIURL
+}
+
+export default axios.create({
+  baseURL: url,
+},
+);
