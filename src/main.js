162ef6af7583df836d89a3871fa057dd34c4e754
Marco Carvalho <marcolucio27@gmail.com>
2018-11-15 18:26:12 -0200

    fix import fontawesome

--- src/main.js
+++ src/main.js
@@ -3,9 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "@fortawesome/fontawesome-svg-core";
-import "@fortawesome/free-solid-svg-icons";
+import {library, dom} from "@fortawesome/fontawesome-svg-core";
+import {fas} from "@fortawesome/free-solid-svg-icons";
 import axios from "axios";
+
+library.add(fas);
+dom.watch();
 
 Vue.config.productionTip = false;
 Vue.prototype.axios = axios;
